2722b1e220d9207e001d9606d4330a5017b45c87
alumno <alumno@LAB00PC00>
2019-08-29 22:01:34 -0300

    “get”

---
+++ Clase 03_V2/src/Clase03_V2.c
@@ -0,0 +1,73 @@
+/*
+ ============================================================================
+ Name        : Get.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+int calculoMaxYMin(int *flagFun,int numeroFun,int *MaxFun, int *MinFun);
+//int getNumero(int* pNumero);
+
+int main(void)
+{
+	int numero;
+	char respuesta;
+	int Max = 0;
+	int Min = 0;
+	int flag = 0;
+
+	do
+	{
+		printf("\nIngrese un numero");
+		scanf("%d",&numero);
+		//getNumero(&numero,&intentos);
+		calculoMaxYMin(&flag,numero,&Max,&Min);
+		printf("\nDesea continuar? s/n");
+		_fpurge(stdin);
+		scanf("%c",&respuesta);
+
+	} while(respuesta != 'n');
+
+	printf("El Maximo es %d ",Max);
+	printf("El Minimo es %d ",Min);
+	return 0;
+}
+
+
+int calculoMaxYMin(int* pFlag,int numeroFun,int* pMax,int* pMin)
+{
+	int max = *pMax;
+	int min = *pMin;
+	int flag = *pFlag;
+
+
+	//pregunto si es la primera vez que entro
+	if(flag==0)
+	{
+		max = numeroFun;
+		min = numeroFun;
+		*pFlag=1; // paso el flag externo a 1
+	}
+
+
+	if(numeroFun > max)
+	{
+		max = numeroFun;
+	}
+	*pMax = max;
+
+
+	if(numeroFun < min)
+	{
+		min = numeroFun;
+	}
+	*pMin = min;
+
+
+	return 0;
+}
+
