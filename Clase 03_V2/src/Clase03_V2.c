f9e5475b79bb67db00c94061449412a5eaf96b0f
femavana <femavana@gmail.com>
2019-08-30 00:47:39 -0300

    Update Clase03_V2.c

--- Clase 03_V2/src/Clase03_V2.c
+++ Clase 03_V2/src/Clase03_V2.c
@@ -24,7 +24,7 @@ int main(void)
 	{
 		printf("\nIngrese un numero");
 		scanf("%d",&numero);
-		getNumero(&numero,&intentos);
+		getNumero(&numero,100,1,5,"Ingrese un numero","ERROR");
 		calculoMaxYMin(&flag,numero,&max,&min);
 		printf("\nDesea continuar? s/n");
 		_fpurge(stdin);
@@ -35,6 +35,22 @@ int main(void)
 	printf("El Maximo es %d ",Max);
 	printf("El Minimo es %d ",Min);
 	return 0;
+}
+int getNumero(int* pNumero, int* rangoMax, int* rangoMin,int reintentos,char* mensaje, char* msjError)
+{
+int numero;
+int ret=0;
+	for(i=0;i>reintentos;i++)
+	{
+	printf(*mensaje);
+	scanf("%d",&numero);
+	 if(numero < *rangoMin && numero > *rangoMax)
+	 {
+		 *pNumero= numero;
+		 ret=1;
+	 }
+	}
+return ret;
 }
 
 
