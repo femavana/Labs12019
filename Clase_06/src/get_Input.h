5d333ff6543630707ead74b2386a2b89c73c1039
femavana <femavana@gmail.com>
2019-09-17 16:45:40 -0300

    Update get_Input.h

--- Clase_06/src/get_Input.h
+++ Clase_06/src/get_Input.h
@@ -56,5 +56,63 @@ int getChar( char *pResultado,
 			 int maximo,
 			 int reintentos);
 
+/**4   \brief Solicita un string al usuario
+     * \param pResultado Puntero del caracter ingresado
+     * \param mensaje  Puntero del mensaje a ser mostrado
+     * \param msjError Puntero del mensaje a ser mostrado en caso de error
+     * \param minimo Limite minimo a validar
+     * \param maximo Limite maximo a validar
+     * \param reintentos Cantidad de intentos en caso de error
+     * \return Devuelve 0 si carga el numero y sino -1
+     */
+int getString( char message[],
+	       char input[])
 
+/**5   \brief Solicita un string al usuario
+     * \param pResultado Puntero del caracter ingresado
+     * \param mensaje  Puntero del mensaje a ser mostrado
+     * \param msjError Puntero del mensaje a ser mostrado en caso de error
+     * \param minimo Limite minimo a validar
+     * \param maximo Limite maximo a validar
+     * \param reintentos Cantidad de intentos en caso de error
+     * \return Devuelve 0 si carga el numero y sino -1
+     */
+int getStringNumbers( char message[],
+		      int  input[])
+	
+/**6   \brief Solicita un string al usuario
+     * \param pResultado Puntero del caracter ingresado
+     * \param mensaje  Puntero del mensaje a ser mostrado
+     * \param msjError Puntero del mensaje a ser mostrado en caso de error
+     * \param minimo Limite minimo a validar
+     * \param maximo Limite maximo a validar
+     * \param reintentos Cantidad de intentos en caso de error
+     * \return Devuelve 0 si carga el numero y sino -1
+     */
+int getStringLetters(char message[],
+		             char msgError[],
+		             char input[])
+/**7   \brief Solicita un string al usuario
+     * \param pResultado Puntero del caracter ingresado
+     * \param mensaje  Puntero del mensaje a ser mostrado
+     * \param msjError Puntero del mensaje a ser mostrado en caso de error
+     * \param minimo Limite minimo a validar
+     * \param maximo Limite maximo a validar
+     * \param reintentos Cantidad de intentos en caso de error
+     * \return Devuelve 0 si carga el numero y sino -1
+     */	
+int OnlyLetters(char aux[],
+            int limite)	
+
+/**8   \brief Solicita un string al usuario
+     * \param pResultado Puntero del caracter ingresado
+     * \param mensaje  Puntero del mensaje a ser mostrado
+     * \param msjError Puntero del mensaje a ser mostrado en caso de error
+     * \param minimo Limite minimo a validar
+     * \param maximo Limite maximo a validar
+     * \param reintentos Cantidad de intentos en caso de error
+     * \return Devuelve 0 si carga el numero y sino -1
+     */
+int OnlyNumbers(int aux)
+	
 #endif /* GET_INPUT_H_ */
