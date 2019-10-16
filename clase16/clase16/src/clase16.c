f27c84acf6a89bf9efaec45dce8db22b0ff8c066
alumno <alumno@LAB00PC00>
2019-10-16 20:23:20 -0300

    clase16

---
+++ clase16/clase16/src/clase16.c
@@ -0,0 +1,126 @@
+/*
+ ============================================================================
+ Name        : clase16.c
+ Author      : Navarro
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
+
+/*
+
+void harcodeoCliente(Clients* client,int cant)
+ {
+
+    strcpy(client[0].nombreEmpresa,"Garbarino");
+    strcpy(client[0].direccion,"Florida 599");
+    strcpy(client[0].localidad,"Capital Federal");
+    strcpy(client[0].cuit,"23-44565467-8");
+    client[0].isEmpty=FALSE;
+    client[0].idCliente=0;
+
+    strcpy(client[1].nombreEmpresa,"Garbarino");
+    strcpy(client[1].direccion,"Florida 599");
+    strcpy(client[1].localidad,"Capital Federal");
+    strcpy(client[1].cuit,"23-44565467-8");
+    client[1].isEmpty=FALSE;
+    client[1].idCliente=1;
+
+    strcpy(client[2].nombreEmpresa,"Garbarino");
+    strcpy(client[2].direccion,"Florida 599");
+    strcpy(client[2].localidad,"Capital Federal");
+    strcpy(client[2].cuit,"23-44565467-8");
+    client[2].isEmpty=FALSE;
+    client[2].idCliente=2;
+
+
+    strcpy(client[3].nombreEmpresa,"Garbarino");
+    strcpy(client[3].direccion,"Florida 599");
+    strcpy(client[3].localidad,"Capital Federal");
+    strcpy(client[3].cuit,"23-44565467-8");
+    client[3].isEmpty=FALSE;
+    client[3].idCliente=3;
+
+    strcpy(client[4].nombreEmpresa,"Garbarino");
+    strcpy(client[4].direccion,"Florida 599");
+    strcpy(client[4].localidad,"Capital Federal");
+    strcpy(client[4].cuit,"23-44565467-8");
+    client[4].isEmpty=FALSE;
+    client4].idCliente=4;
+
+    strcpy(client[5].nombreEmpresa,"Garbarino");
+    strcpy(client[5].direccion,"Florida 599");
+    strcpy(client[5].localidad,"Capital Federal");
+    strcpy(client[5].cuit,"23-44565467-8");
+    client[5].isEmpty=FALSE;
+    client[5].idCliente=5;
+
+    strcpy(client[6].nombreEmpresa,"Garbarino");
+    strcpy(client[6].direccion,"Florida 599");
+    strcpy(client[6].localidad,"Capital Federal");
+    strcpy(client[6].cuit,"23-44565467-8");
+    client[6].isEmpty=FALSE;
+    client[6].idCliente=6;
+
+    strcpy(client[7].nombreEmpresa,"Garbarino");
+    strcpy(client[7].direccion,"Florida 599");
+    strcpy(client[7].localidad,"Capital Federal");
+    strcpy(client[7].cuit,"23-44565467-8");
+    client[7].isEmpty=FALSE;
+    client[7].idCliente=7;
+
+ }
+
+
+void harcodeoPedido(Pedidos* pedido,int size)
+ {
+    pedido[0].cantRecoleccion=45.5;
+    pedido[0].estado=ESTADO_PENDIENTE;
+    pedido[0].isEmpty=FALSE;
+    pedido[0].idPedido=client[0].idCliente;
+
+    pedido[1].cantRecoleccion=9.2;
+    pedido[1].estado=ESTADO_PENDIENTE;
+    pedido[1].isEmpty=FALSE;
+    pedido[1].idPedido=client[1].idCliente;
+
+    pedido[2].cantRecoleccion=14.2;
+    pedido[2].estado=ESTADO_PENDIENTE;
+    pedido[2].isEmpty=FALSE;
+    pedido[2].idPedido=client[2].idCliente;
+
+    pedido[3].cantRecoleccion=89.9;
+    pedido[3].estado=ESTADO_PENDIENTE;
+    pedido[3].isEmpty=FALSE;
+    pedido[3].idPedido=client[3].idCliente;
+
+    pedido[4].cantRecoleccion=54.2;
+    pedido[4].estado=ESTADO_PENDIENTE;
+    pedido[4].isEmpty=FALSE;
+    pedido[4].idPedido=client[4].idCliente;
+
+    pedido[5].cantRecoleccion=95.3;
+    pedido[5].estado=ESTADO_PENDIENTE;
+    pedido[5].isEmpty=FALSE;
+    pedido[5].idPedido=client[5].idCliente;
+
+    pedido[6].cantRecoleccion=80.5;
+    pedido[6].estado=ESTADO_PENDIENTE;
+    pedido[6].isEmpty=FALSE;
+    pedido[6].idPedido=client[6].idCliente;
+
+    pedido[7].cantRecoleccion=50.5;
+    pedido[7].estado=ESTADO_PENDIENTE;
+    pedido[7].isEmpty=FALSE;
+    pedido[7].idPedido=client[7].idCliente;
+ }
+ */
