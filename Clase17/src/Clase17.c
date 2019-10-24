d7da879e4df81c3295feb64da6a0a014056a77e7
alumno <alumno@LAB00PC00>
2019-10-23 21:58:33 -0300

    Tp2 con punteros

---
+++ Clase17/src/Clase17.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Clase17.c
+ Author      : Navarro
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Archivos"); /* prints Archivos */
+	return EXIT_SUCCESS;
+}
